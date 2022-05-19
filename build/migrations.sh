561912aa1c4903a9d10239e00b504ba0ad2f88f4
root <zayyadidris294@gmail.com>
2022-05-19 15:42:32 +0000

    dfe

--- build/migrations.sh
+++ build/migrations.sh
@@ -3,4 +3,3 @@ python3 manage.py flush --no-input
 python3 manage.py migrate
 python3 manage.py makemigrations app
 python3 manage.py migrate app
-python3 admin.py
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -3,14 +3,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DEBUG') )== "1"
+DEBUG = str(os.environ.get('DEBUG', 'False')) == '1'
 ALLOWED_HOSTS = ['*']
 
 
@@ -60,14 +56,15 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "portfolio",
-        "USER": "zayyad",
-        "PASSWORD": "zayyad",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": "127.0.0.1",
-        "PORT": "5433",
+        "PORT": "5432",
     }
 }
 
